54c78c06a8e55391a1865f42346ac0cd069b8622
Nantha333 <fftnantha007@gmail.com>
2025-01-26 22:09:30 +0530

    Add files via upload

---
+++ javascript/function.js
@@ -0,0 +1,41 @@
+function add()
+{
+    var a=10
+    var b=10
+    console.log(a+b)
+}
+add()
+
+//------------------------------------
+
+function actor()
+{
+    console.log("favourite actor kamal")
+}
+actor()
+
+function player()
+{
+    console.log("favourite player Dhoni")
+}
+player()
+
+function movie()
+{
+    console.log("favourite actor ambesivam")
+}
+movie()
+
+
+var factor="kamal"
+console.log("favourite actor:" + factor)
+
+//----------------------
+
+function add(a,b)
+{
+    return (a+b)
+ 
+}
+var a=add(10+20)
+console.log(a)
---
+++ javascript/ifelse.js
@@ -0,0 +1,74 @@
+var rain=true
+
+if(rain)
+{
+    console.log("take an umberlla")
+}
+else{
+    console.log("Enjoy the sun shine")
+}
+
+//------------------
+
+var homework=false
+
+if(homework)
+{
+    console.log("great job")
+}
+else{
+    console.log("finish your homework")
+}
+
+//-------
+
+var color = "yellow"
+
+if(color == "red")
+{
+   console.log("stop")
+}
+if(color == "green")
+    {
+       console.log("go")
+    }
+
+    if(color == "yellow")
+        {
+           console.log("wait")
+        }
+
+
+//------------------------------------------------
+
+var score = 70
+
+if(score<=50)
+{
+    console.log("improve")
+}
+
+if(score>50 && score<70)
+{
+    console.log("good job")
+}
+
+//------------------------------------------------
+
+for(count=1; count<=5; count=count+1)
+{
+    console.log("Nantha")
+}
+
+for(count=10; count>=1; count=count-1)
+    {
+        console.log(count)
+    }
+    
+
+    for(count=1; count<=10; count=count+1)
+        {
+            console.log(count+"*2="+ (count*2))
+        }
+        
+//------------------------------------------------------------
